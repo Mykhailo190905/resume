860a53c032552886470dca7a5bcaeaf00eaf0824
Mykhailo190905 <mykhailobondarenko280@gmail.com>
2024-11-28 12:11:16 +0100

    changes

--- cod.js
+++ cod.js
@@ -2,3 +2,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Резюме загружено. Добро пожаловать!");
 });
+
+document.getElementById('show-goal').addEventListener('click', function() {
+    const goalSection = document.getElementById('goal-section');
+    if (goalSection.style.display === 'none' || goalSection.style.display === '') {
+        goalSection.style.display = 'block'; // Показываем цель
+        this.textContent = 'Hide Goal'; // Меняем текст кнопки
+    } else {
+        goalSection.style.display = 'none'; // Скрываем цель
+        this.textContent = 'Show Goal'; // Меняем текст кнопки
+    }
+});
